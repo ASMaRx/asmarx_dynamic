842d9ffe9b0e1f4ca9ab3752a429b6c9a4d9e5c8
roxerg <roxguda@gmail.com>
2021-04-15 14:12:48 +0100

    fixed display

---
+++ src/components/MarxHeadSmall.vue
@@ -0,0 +1,21 @@
+
+<template>
+
+    <div>
+    <div class="marxhead">
+            <g-image class="marximg-ep head" src="~/images/head.png" />
+            <g-image class="marximg-ep z"    src="~/images/z.png" />
+            <g-image class="marximg-ep zz"   src="~/images/zz.png" />
+            <g-image class="marximg-ep zzz"  src="~/images/zzz.png" />
+    </div>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        name: 'MarxHead'
+    }
+
+</script>
